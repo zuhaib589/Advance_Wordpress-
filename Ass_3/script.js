cd5b6ce646154ae971fa1dcfdf44f314cb94637a
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-03 15:53:24 +0500

    Ass-3-js-1

---
+++ Ass_3/script.js
@@ -0,0 +1,29 @@
+
+// 1 Create two variables, num1 and num2, and assign them any two numbers of your choice.
+// Perform Arithmetic Operations:
+// Add, subtract, multiply, and divide num1 and num2. Store the results in separate variables named sum, difference, product, and quotient.
+// Display Results:
+// Print the values of sum, difference, product, and quotient.
+
+// Declare Variable
+// how to declare variable 
+
+// vaiable name = value 
+
+num1=10
+num2=20
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+console.log("10 + 20 =" , add);
+console.log("10 - 20 =" , sub);
+console.log("10 x 20 =" , mul);
+console.log("10 / 20 =" , div)
+
+
+
+
+
+
