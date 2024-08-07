5b4aa0178e70b79fe844ec8c0cde7bdc5a6d7389
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-07 16:28:48 +0500

    if-else

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// console.log("hello world");
+
+
+
+let  day= "tuesday"
+
+if (day == "Monday") {
+console.log("Today is monday ");
+}
+
+else{ 
+     console.log("no day ");   
+}
+             
+
