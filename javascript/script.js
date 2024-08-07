6f9895bd776e59aa6ec682c234f7b810cc471223
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-07 23:01:31 +0500

    conditional statement

--- javascript/script.js
+++ javascript/script.js
@@ -2,14 +2,50 @@
 
 
 
-let  day= "tuesday"
+// let  day= "tuesday"
 
-if (day == "Monday") {
-console.log("Today is monday ");
-}
+// if (day == "Monday") {
+// console.log("Today is monday ");
+// }
 
-else{ 
-     console.log("no day ");   
-}
+// else{ 
+//      console.log("no day ");   
+// }
              
+// switch statement 
+
+// let day = "tuesday"
+// switch(day){
+//      case "Monday":
+//       console.log("Today is monday");
+//       break ; 
+//      case "tuesday":
+//           console.log("Today is tuesday");
+//           break ;
+//      case "wednesday":
+//                console.log("Today is wednesday");        
+//                break ;  
+     
+// }
+
+// #for loop
+
+// name1="zuhaib" use variable for dyanmically
+// console.log(`hello ${name1} how are you 1`);
+
+
+// for (let i = 0; i <10; i++) {
+//      console.log("hello zuhaib" + (i+1));
+     
+     
+// }
+
+// array
+
+food=["Breakfast" , "lunch", "dinner"]
+lastfood=food[food.length - 1]
+console.log(lastfood);
+
+
+// object
 
